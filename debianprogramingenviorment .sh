8ad19af417c2ca9eb4f5feabd2733967f540e63b
A B Shravan Krishna <77732894+abshravan@users.noreply.github.com>
2023-05-18 15:38:37 +0530

    Update debianprogramingenviorment .sh

--- debianprogramingenviorment .sh
+++ debianprogramingenviorment .sh
@@ -6,6 +6,6 @@ sudo apt update
 sudo apt install git -y 
 #Installing Text editor
 echo "Installing the text editor"
-sudo snap install code -y
+sudo snap install code 
 #Installing and updating gcc compiler
 sudo apt install -y
