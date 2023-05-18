86d7b489e760b443aae781cfca63d5c9134c057b
A B Shravan Krishna <77732894+abshravan@users.noreply.github.com>
2023-05-18 15:19:41 +0530

    Add files via upload

---
+++ debianprogramingenviorment .sh
@@ -0,0 +1,11 @@
+echo"This script contains installation and setting up of c and c++ programming enviorment \n
+Loading .........."
+#Updating the package manager 
+sudo apt update 
+#Installing git 
+sudo apt install git -y 
+#Installing Text editor
+echo "Installing the text editor"
+sudo apt install code -y
+#Installing and updating gcc compiler
+sudo apt install -y
